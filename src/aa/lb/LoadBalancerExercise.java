0ab7d7d35d4bab522937739dd7a34e4819aa218d
Ziming Lan <ziming.lan@gmail.com>
2015-09-10 22:26:00 +0800

    some new load balance method added.

--- src/aa/lb/LoadBalancerExercise.java
+++ src/aa/lb/LoadBalancerExercise.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 public class LoadBalancerExercise {
 
+    private final int PATTERN_CHOICE = Service.PATTERN_2;
     Service[] workers;
     private int afterSpikeCounter;
-    private boolean spikeFlag = false;
+    private Random random;
 
     public LoadBalancerExercise(int numWorkers) {
 
@@ -14,13 +15,15 @@ public class LoadBalancerExercise {
 
         for (int i = 0; i < numWorkers; i++) {
             //Service has three load patterns: EASY_PATTERN, PATTERN_2 (Spike), PATTERN_3 (Cache)
-            workers[i] = new Service(String.format("worker %d", i + 1), Service.PATTERN_2);
+            workers[i] = new Service(String.format("worker %d", i + 1), PATTERN_CHOICE);
         }
+        random = new Random();
 
     }
 
     public static void main(String[] args) throws InterruptedException {
-        //To specify number of workers
+
+        // Specify number of workers in LoadBalancerExercise constructor
         LoadBalancerExercise lbe = new LoadBalancerExercise(2);
         lbe.go();
     }
@@ -35,16 +38,19 @@ public class LoadBalancerExercise {
      */
 
     private void balance(int requestID, int requestParameter) {
+
+        // See the respective balance methods
+
         //The given code has no balancing - everything goes to worker_1.
 
 
         // currently a basic round robin algorithm, change it to your liking! 12854, 12787
-        if (requestID % 250 == 0) {
-            workers[0].service(requestID, requestParameter);
-            spikeFlag = true;
-        } else if (spikeFlag) {
-            workers[1].service(requestID, requestParameter);
-        }
+//        if (requestID % 250 == 0) {
+//            workers[0].service(requestID, requestParameter);
+//            spikeFlag = true;
+//        } else if (spikeFlag) {
+//            workers[1].service(requestID, requestParameter);
+//        }
 
 //        workers[requestID % workers.length].service(requestID, requestParameter);
 
@@ -75,12 +81,50 @@ public class LoadBalancerExercise {
 
     }
 
-    private void roundRobinBalance(int requestID, int requestParameter) {
+    private void basicRoundRobinBalance(int requestID, int requestParameter) {
         workers[requestID % workers.length].service(requestID, requestParameter);
     }
 
     private void randomBalance(int requestID, int requestParameter) {
-        workers[(int) (Math.random() * workers.length)].service(requestID, requestParameter);
+        workers[random.nextInt(workers.length)].service(requestID, requestParameter);
+    }
+
+    private void weightedRoundRobinBalance(int requestID, int requestParameter) {
+
+    }
+
+    private void evenSizeTaskQueueBalance(int requestID, int requestParameter) {
+
+        // get the worker with lowest queue size
+        Service selectedWorker = getWorkerWithLowestQueueSize();
+        selectedWorker.service(requestID, requestParameter);
+    }
+
+    private Service getWorkerWithLowestQueueSize() {
+
+
+        // this 1 if only 2 worker
+//        return (workers[0].currentQueueSize() < workers[1].currentQueueSize()) ? workers[0] : workers[1];
+
+
+        // this 1 for any amount of workers.
+        // aim for clarity, though when the worker count get large, the code may need to be improved.
+
+        int leastTasksIndex = 0;
+
+        for (int i = 0; i < workers.length; i++) {
+
+            int currentWorkerQueueSize = workers[i].currentQueueSize();
+            int currentLeastTasksWorkerQueueSize = workers[leastTasksIndex].currentQueueSize();
+
+            if (currentWorkerQueueSize < currentLeastTasksWorkerQueueSize) {
+                leastTasksIndex = i;
+            }
+
+        }
+
+        return workers[leastTasksIndex];
+
     }
 
     /**
@@ -114,13 +158,19 @@ public class LoadBalancerExercise {
         //total number of "requests" to process.  You can play with this, but it shouldn't make much difference.
 
 //        int numRequests = 1000;
-        int numRequests = 15;
+        int numRequests = 1000;
 
         for (int requestID = 0; requestID < numRequests; requestID++) {
 //            Thread.sleep(4);
-            int requestParameter = new Random().nextInt(5); //there are different values the client can send in the request; this represents "search for ..."
+            int requestParameter = random.nextInt(5); //there are different values the client can send in the request; this represents "search for ..."
 
-            roundRobinBalance(requestID, requestParameter);
+            // uncomment and test
+
+            basicRoundRobinBalance(requestID, requestParameter);
+//            randomBalance(requestID, requestParameter);
+
+//            evenSizeTaskQueueBalance(requestID, requestParameter);
+
         }
 
         //workers will stop after all requests are processed
