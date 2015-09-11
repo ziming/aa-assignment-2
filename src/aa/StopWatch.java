30fe275707bd90d4caac2a53f40d6a08230982d3
Ziming Lan <ziming.lan@gmail.com>
2015-09-12 00:25:56 +0800

    stop watch class

--- src/aa/StopWatch.java
+++ src/aa/StopWatch.java
@@ -3,25 +3,23 @@ package aa;
 /**
  * Created by kevinsteppe on 4/8/15.
  */
-public class StopWatch
-{
+public class StopWatch {
     private long start = 0;
     private long end = 0;
 
     /**
      * Starts the watch.
      */
-    public void start()
-    {
+    public void start() {
         start = System.currentTimeMillis();
     }
 
     /**
      * Stops the watch, then returns the elapsed time.
+     *
      * @return
      */
-    public long stop()
-    {
+    public long stop() {
         end = System.currentTimeMillis();
         return getTime();
     }
@@ -29,16 +27,15 @@ public class StopWatch
     /**
      * If end() has been called, then returns milliseconds elapsed between start and end.
      * If end() has not been called yet, returns milliseconds since start was called (does not stop the watch)
+     *
      * @return
      */
-    public long getTime()
-    {
+    public long getTime() {
         if (end == 0) return System.currentTimeMillis() - start;
-        else return (end-start);
+        else return (end - start);
     }
 
-    public void reset()
-    {
+    public void reset() {
         start = end = 0;
     }
 
