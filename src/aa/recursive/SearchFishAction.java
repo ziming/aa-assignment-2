86ced32dc06c32dd53a1788b965f014d5b920ae3
Ziming Lan <ziming.lan@gmail.com>
2015-09-12 22:52:32 +0800

    mostly works now, do play with it.

--- src/aa/recursive/SearchFishAction.java
+++ src/aa/recursive/SearchFishAction.java
@@ -1,32 +1,49 @@
 package aa.recursive;
 
-import aa.StopWatch;
-import com.univocity.parsers.annotations.Parsed;
-import com.univocity.parsers.common.processor.BeanListProcessor;
 import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
 
 import java.io.*;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
 
 public class SearchFishAction extends RecursiveAction {
 
+    private static final int LIMIT = 500_000;
+    // CSV Columns
+    // TradeID, BuyerID, SellerID, Fish Type, Price, Number of Fish traded
+    private static final int TRADE_ID = 0;
+    private static final int BUYER_ID = 1;
+    private static final int SELLER_ID = 2;
+    private static final int FISH_TYPE = 3;
+    private static final int PRICE = 4;
+    private static final int NUMBER_OF_FISH_TRADED = 5;
+    private List<String[]> fishList;
+
+    public SearchFishAction(List<String[]> fishList) {
+        this.fishList = fishList;
+    }
+
     public static void main(String[] args) {
 
 
         // Stop Watch Start!
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
+//        StopWatch stopWatch = new StopWatch();
+//        stopWatch.start();
 
         // Create empty list for all fishes.
-        List<String[]> allFishes = new LinkedList<>();
+        List<String[]> fishList = new ArrayList<>(1_500_000);
 
         // 1. Read all the files and add them to a list.
         CsvParserSettings settings = new CsvParserSettings();
         settings.getFormat().setLineSeparator("\n");
+
+        // By default readInputOnSeparateThread is true, for the purpose of this problem
+        // let's set it to false. So that it does not use multi threading.
+        settings.setReadInputOnSeparateThread(false);
 
         // creates a CSV parser
         CsvParser parser = new CsvParser(settings);
@@ -36,56 +53,27 @@ public class SearchFishAction extends RecursiveAction {
 
             // on the 8th file fish7.dat the program seem to hang :(
             for (int i = 0; i < 1; i++) {
-                allFishes.addAll(parser.parseAll(getReader(String.format("fish%d.dat", i))));
+                fishList.addAll(parser.parseAll(getReader(String.format("fish%d.dat", i))));
             }
 
-            System.out.println(allFishes.size());
 
         } catch (FileNotFoundException | UnsupportedEncodingException e) {
             e.printStackTrace();
         }
 
-        System.out.printf("Time taken (ms): %d\n", stopWatch.stop());
+        System.out.println(fishList.size());
 
+        SearchFishAction searchFishAction = new SearchFishAction(fishList);
 
-//        SearchFishAction searchFishAction = new SearchFishAction();
-//        ForkJoinPool forkJoinPool = new ForkJoinPool(1);
-//        forkJoinPool.invoke(searchFishAction);
+        int forkJoinPoolSize = 1;
+        ForkJoinPool forkJoinPool = new ForkJoinPool(forkJoinPoolSize);
+        forkJoinPool.invoke(searchFishAction);
+
         // searchFishAction.go();
     }
 
     public static Reader getReader(String fileName) throws UnsupportedEncodingException, FileNotFoundException {
         return new InputStreamReader(new FileInputStream(fileName), "UTF-8");
-    }
-
-    public void go() {
-
-        BeanListProcessor<FishBean> rowProcessor = new BeanListProcessor<>(FishBean.class);
-
-        CsvParserSettings parserSettings = new CsvParserSettings();
-        CsvParser parser = new CsvParser(parserSettings);
-        parserSettings.setRowProcessor(rowProcessor);
-
-        // might be too big and run out of memory 1.5 million lines per file after all
-        // if fail look at real all rows of a csv iterator style
-        try {
-            parser.parseAll(getReader("../../fish0.dat"));
-            List<FishBean> fishBeans = rowProcessor.getBeans();
-
-            for (FishBean fishBean : fishBeans) {
-
-                // Getting prices of stingrays only
-                if (fishBean.getPrice() >= 9000) {
-                    if (fishBean.getFishType().equals("sting ray")){
-                        System.out.println("TradeID: " + fishBean.getTradeID() + "- score!");
-                    }
-                }
-            }
-
-        } catch (UnsupportedEncodingException | FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
     }
 
     /**
@@ -96,41 +84,33 @@ public class SearchFishAction extends RecursiveAction {
 
         // if problem too small just do it yourself
 
-        /*
-        if (true) {
-            // search for stuff
-            return;
-        }
-        */
+        int fishListSize = fishList.size();
+        if (fishListSize <= LIMIT) {
 
-        // otherwise
-        // invokeAll(...)
+            for (String[] fishRow : fishList) {
+
+                if (fishRow[FISH_TYPE].equals("sting ray")) {
+
+                    // assumption it will all be proper integer strings, hence no exception
+                    int fishPrice = Integer.parseInt(fishRow[PRICE]);
+
+                    if (fishPrice > 9000) {
+                        System.out.println("score");
+                        return;
+                    }
+
+                }
+            }
+
+        } else {
+
+            // hmmm
+            invokeAll(
+                    new SearchFishAction(fishList.subList(0, fishListSize / 2)),
+                    new SearchFishAction(fishList.subList(fishListSize / 2, fishListSize))
+            );
+        }
+
     }
 
-    class FishBean {
-        @Parsed(index = 0)
-        private String tradeID;
-
-        @Parsed(index = 3)
-        private String fishType;
-
-        @Parsed(index = 4)
-        private Integer price;
-
-        public String getTradeID() {
-            return tradeID;
-        }
-
-        public String getFishType() {
-            return fishType;
-        }
-
-        public Integer getPrice() {
-            return price;
-        }
-
-        public String toString() {
-            return "FishBean [tradeID=" + tradeID + ", fishType=" + fishType + ", price=" + price + "]";
-        }
-    }
 }
